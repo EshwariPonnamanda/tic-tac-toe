a0c37e5be6aa62a9964fb0e9d61bbf04fc12ba25
EshwariPonnamanda <eshwariponnamanda@gmail.com>
2025-06-17 21:00:46 -0400

    Improve board

--- Board.java
+++ Board.java
@@ -1,39 +1,42 @@
-public class Board{
-    private static final int SIZE=3;
+public class Board {
+    private static final int SIZE = 3;
     private Cell[][] grid;
 
-    public Board(){
+    public Board() {
         grid = new Cell[SIZE][SIZE];
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 grid[i][j] = new Cell();
             }
         }
     }
 
-    public void printBoard(){
-        System.out.println("\nCurrent Board:");
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
-                System.out.print(" "+ grid[i][j].getValue()+" ");
-                if(j<SIZE-1) System.out.println("---+---+---");
-            }
-            System.out.println();
-            if (i < SIZE - 1) System.out.println("---+---+---");
+    public void printBoard() {
+    System.out.println("\n    0   1   2");
+    for (int i = 0; i < SIZE; i++) {
+        System.out.print(" " + i + " ");
+        for (int j = 0; j < SIZE; j++) {
+            System.out.print(" " + grid[i][j].getValue() + " ");
+            if (j < SIZE - 1) System.out.print("|");
         }
+        System.out.println();
+        if (i < SIZE - 1) System.out.println("   ---+---+---");
     }
+}
 
-        public boolean placeSymbol(int row, int col, char symbol){
-            if(row<0 || row>= SIZE || col<0 || col>=SIZE){
-                return false;
-            }
-            if (!grid[row][col].isEmpty()) {
+    public boolean placeSymbol(int row, int col, char symbol) {
+        if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) {
+            System.out.println("Invalid position!");
+            return false;
+        }
+        if (!grid[row][col].isEmpty()) {
             System.out.println("Cell already taken!");
             return false;
         }
         grid[row][col].setValue(symbol);
         return true;
     }
+
     public boolean isFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
